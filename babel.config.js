af156b9e95359eaec2514fe32ab314778260ecc7
Kennie Davis <kenniejaydavis@gmail.com>
2021-06-10 11:15:54 -0700

    setup dependency injection

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  plugins: [
+    'babel-plugin-transform-typescript-metadata',
+    'react-native-reanimated/plugin',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ],
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
 };
--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { P9DependencyContainerProvider } from './core/di';
 import { P9ThemeProvider } from './core/theme';
 import { P9AuthorizationProvider } from './features/authorization';
 import { P9NavigationContainer } from './features/navigation';
@@ -12,7 +13,9 @@ export const P9App = () => {
       <P9AuthorizationProvider>
         <P9PartitionProvider>
           <P9ThemeProvider>
-            <P9NavigationContainer />
+            <P9DependencyContainerProvider>
+              <P9NavigationContainer />
+            </P9DependencyContainerProvider>
           </P9ThemeProvider>
         </P9PartitionProvider>
       </P9AuthorizationProvider>
