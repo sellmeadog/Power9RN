abdb6705acd8227d56735505cd61d61482e38173
Kennie Davis <kenniejaydavis@gmail.com>
2021-06-15 16:05:03 -0700

    update unit tests

--- src/features/magic-card-filter/model/serialization.spec.ts
+++ src/features/magic-card-filter/model/serialization.spec.ts
@@ -3,24 +3,22 @@ import { serializeColorPredicate } from './serialization';
 describe('Predicate Serialization', () => {
   it('should serialize exact color predicate expression', () => {
     const expected =
-      'AND card_faces.colors =[c] "W" AND card_faces.colors =[c] "U" AND NOT card_faces.colors =[c] "B" AND NOT card_faces.colors =[c] "R" AND NOT card_faces.colors =[c] "G" AND NOT card_faces.colors =[c] "C"';
+      '(card_faces.colors =[c] "W" AND card_faces.colors =[c] "U" AND NOT card_faces.colors =[c] "B" AND NOT card_faces.colors =[c] "R" AND NOT card_faces.colors =[c] "G" AND NOT card_faces.colors =[c] "C")';
 
     const actual = serializeColorPredicate({
       attribute: 'card_faces.colors',
-      expression: { fuzziness: 0, W: true, U: true },
-      id: 'card_faces.colors',
+      predicates: { fuzziness: 0, W: true, U: true },
     });
 
     expect(actual).toEqual(expected);
   });
 
   it('should serialize include color predicate expression', () => {
-    const expected = 'AND card_faces.colors =[c] "W" AND card_faces.colors =[c] "U"';
+    const expected = '(card_faces.colors =[c] "W" AND card_faces.colors =[c] "U")';
 
     const actual = serializeColorPredicate({
       attribute: 'card_faces.colors',
-      expression: { fuzziness: 1, W: true, U: true },
-      id: 'card_faces.colors',
+      predicates: { fuzziness: 1, W: true, U: true },
     });
 
     expect(actual).toEqual(expected);
@@ -28,12 +26,11 @@ describe('Predicate Serialization', () => {
 
   it('should serialize at most color predicate expression', () => {
     const expected =
-      'AND (card_faces.colors =[c] "W" OR card_faces.colors =[c] "U" OR card_faces.colors.@count == 0) AND (NOT card_faces.colors =[c] "B" AND NOT card_faces.colors =[c] "R" AND NOT card_faces.colors =[c] "G" AND NOT card_faces.colors =[c] "C")';
+      '(card_faces.colors =[c] "W" OR card_faces.colors =[c] "U" OR card_faces.colors.@count == 0 AND NOT card_faces.colors =[c] "B" AND NOT card_faces.colors =[c] "R" AND NOT card_faces.colors =[c] "G" AND NOT card_faces.colors =[c] "C")';
 
     const actual = serializeColorPredicate({
       attribute: 'card_faces.colors',
-      expression: { fuzziness: 2, W: true, U: true },
-      id: 'card_faces.colors',
+      predicates: { fuzziness: 2, W: true, U: true },
     });
 
     expect(actual).toEqual(expected);
@@ -43,8 +40,7 @@ describe('Predicate Serialization', () => {
     const expected = '';
     const actual = serializeColorPredicate({
       attribute: 'card_faces.colors',
-      expression: { fuzziness: 2, W: false, U: false, B: false, R: false, G: false, C: false },
-      id: 'card_faces.colors',
+      predicates: { fuzziness: 2, W: false, U: false, B: false, R: false, G: false, C: false },
     });
 
     expect(actual).toEqual(expected);
