2044d995ebfa035959fa49133094190460c848d9
Kennie Davis <kenniejaydavis@gmail.com>
2021-07-08 22:05:51 -0700

    allow updating public data from dev screen * only used while in development mode

--- src/features/developer/developer-screen/developer.screen.tsx
+++ src/features/developer/developer-screen/developer.screen.tsx
@@ -4,22 +4,29 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { useNavigation } from '@react-navigation/core';
 
+import { P9TableDivider } from '../../../components';
+import { useDependency } from '../../../core/di';
+import { P9PublicPartitionService } from '../../../core/public/state/public-partition.service';
 import { useAuthorizationFacade } from '../../authorization';
 
 export interface P9DeveloperScreenProps {}
 
 export const P9DeveloperScreen: FunctionComponent<P9DeveloperScreenProps> = () => {
+  const service = useDependency(P9PublicPartitionService);
   const [{ user, isAnonymous }] = useAuthorizationFacade();
   const { navigate } = useNavigation();
 
   return (
     <SafeAreaView>
       <Text>{'Developer Settings'}</Text>
+      <P9TableDivider title={'Security'} />
       <Text>
         User: {user?.profile.email} ({user?.id})
       </Text>
       <Text>isAnonymous: {isAnonymous ? 'True' : 'False'}</Text>
       <Button onPress={() => navigate('P9:Authorization')} title={'Authenticate'} />
+      <P9TableDivider title={'Public Data'} />
+      <Button onPress={() => service.etl()} title={'Populate Public Partition'} />
     </SafeAreaView>
   );
 };
