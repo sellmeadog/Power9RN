0356d5d0a0e0204383c277dec52b45f0750a7ad1
Kennie Davis <kenniejaydavis@gmail.com>
2021-07-13 12:58:10 -0700

    make sure results data provider calls tojson

--- src/core/realm/results-data-provider.tsx
+++ src/core/realm/results-data-provider.tsx
@@ -1,33 +1,14 @@
-import { Results } from 'realm';
 import { BaseDataProvider } from 'recyclerlistview';
 
+export type P9DataObject = { _id: string } & Realm.Object;
+
 export class ResultsDataProvider extends BaseDataProvider {
-  results: Results<any> | undefined;
+  getDataForIndex = (index: number): P9DataObject => this.getAllData()[index].toJSON();
 
-  constructor(public rowHasChanged: (r1: any, r2: any) => boolean) {
-    super(rowHasChanged, (index) => index.toString());
+  public newInstance(
+    rowHasChanged: (r1: any, r2: any) => boolean,
+    getStableId?: ((index: number) => string) | undefined,
+  ): BaseDataProvider {
+    return new ResultsDataProvider(rowHasChanged, getStableId);
   }
-
-  getDataForIndex = (index: number) => this.getAllData()[index];
-
-  getAllData = () => (this.results?.isValid?.() ? (this.results as unknown as any[]) : ([] as any[]));
-
-  getSize = () => this.getAllData().length;
-
-  hasStableIds = () => true;
-
-  requiresDataChangeHandling = () => true;
-
-  getFirstIndexToProcessInternal = () => 0;
-
-  getStableId = (index: number) => index.toString();
-
-  newInstance = (rowHasChanged: (r1: any, r2: any) => boolean) => new ResultsDataProvider(rowHasChanged);
-
-  cloneWithRows = (data: any[]) => {
-    const dp = new ResultsDataProvider(this.rowHasChanged);
-    dp.results = data as unknown as Results<any>;
-
-    return dp;
-  };
 }
--- src/features/decklist-editor/state/decklist-editor.query.ts
+++ src/features/decklist-editor/state/decklist-editor.query.ts
@@ -4,6 +4,7 @@ import { singleton } from 'tsyringe';
 import { QueryEntity } from '@datorama/akita';
 
 import { P9UserDecklistFeatureState, P9UserDecklistFeatureStore } from '../../decklist-explorer/state';
+import { P9MagicCardGalleryQuery } from '../../magic-cards/state/magic-card.query';
 import { P9DecklistEditorState } from '../decklist-editor.model';
 
 @singleton()
@@ -11,9 +12,10 @@ export class P9DecklistEditorQuery extends QueryEntity<P9UserDecklistFeatureStat
   editorState$: Observable<P9DecklistEditorState> = combineLatest({
     activeEntryType: this.select(({ ui }) => ui.decklistEditorState?.activeEntryType),
     name: this.selectActive(({ name }) => name),
+    entries: this.selectActive(({ entries }) => entries),
   });
 
-  constructor(store: P9UserDecklistFeatureStore) {
+  constructor(store: P9UserDecklistFeatureStore, private magicCardQuery: P9MagicCardGalleryQuery) {
     super(store);
   }
 }
