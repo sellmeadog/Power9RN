3d1ccffe85fbd37ef9e0944115ad8b502a8b78d4
Kennie Davis <kenniejaydavis@gmail.com>
2021-08-28 09:40:20 -0700

    ignore auth0 warning

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import 'react-native-get-random-values';
 import 'reflect-metadata';
 
+import { LogBox } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 
 import { persistState } from './core/storage';
 
 enableScreens();
 persistState();
+
+LogBox.ignoreLogs(['The Bundle Identifier or Application ID']);
 
 export * from './app';
