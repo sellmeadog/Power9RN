6920e052b530949249260f27790832ee6bdb3e22
Kennie Davis <kenniejaydavis@gmail.com>
2021-09-12 13:47:46 -0700

    rename react native project

--- ios/Power9Tests/Power9Tests.m
+++ ios/Power9Tests/Power9Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Power9RNTests : XCTestCase
+@interface Power9Tests : XCTestCase
 
 @end
 
-@implementation Power9RNTests
+@implementation Power9Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
